bb0dae36c7b9b5e1b69ba5c0d968307ac9fa40ee
Praise Olayemi <funmilayo1302@gmail.com>
2023-06-19 19:49:23 -0700

    add decsription to betty documentation style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
  * main - Entry point
  *
  * Return: 0(success)
+ * Description - prints aline
  */
-int main(void){
+int main(void)
+{
 
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
