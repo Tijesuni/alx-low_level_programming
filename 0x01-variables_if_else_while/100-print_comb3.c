6987c395380eaf87cc5158d3243dc493fab6e13e
Praise Olayemi <funmilayo1302@gmail.com>
2023-06-25 20:48:05 -0700

    prints combination without repetition

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of two digit
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i >= j)
+			{
+				continue;
+			}
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
