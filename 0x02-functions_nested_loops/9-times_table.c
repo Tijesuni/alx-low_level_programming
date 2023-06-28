f9abea493459c6273b90b6742741da7396f5a28a
Praise Olayemi <funmilayo1302@gmail.com>
2023-06-28 00:04:17 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,17 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, timesNo;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		for (j = 1; j < 10; j++)
 		{
-			int timesNo = i * j;
+			_putchar(',');
+			_putchar(' ');
+
+			timesNo = i * j;
 
 			if (timesNo < 10)
 			{
@@ -26,8 +30,6 @@ void times_table(void)
 				_putchar((timesNo / 10) + '0');
 			}
 			_putchar((timesNo % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
