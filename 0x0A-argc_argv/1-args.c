2be619011eb3d3f14552c429a3de6bfce73a56fb
Praise Olayemi <funmilayo1302@gmail.com>
2023-07-26 08:46:19 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ * @argc: The number of argument
+ * @argv: The one-dimensional array
+ * Return: it always 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	(void)**argv;
+	return (0);
+}
