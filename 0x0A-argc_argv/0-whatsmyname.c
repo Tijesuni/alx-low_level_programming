724a9370b535c64eb217f885559f2f8237d02485
Praise Olayemi <funmilayo1302@gmail.com>
2023-09-01 17:09:32 -0700

    Print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of argument
+ * @argv: The one-dimensional array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
