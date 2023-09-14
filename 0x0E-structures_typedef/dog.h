79c72c7a6d90476b9ac356e7c27e9caf8c2f9344
Praise Olayemi <funmilayo1302@gmail.com>
2023-09-14 05:35:11 -0700

    Add description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - contains a dog information
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog'd owner
+ */
+
 struct dog
 {
 	char *name;
