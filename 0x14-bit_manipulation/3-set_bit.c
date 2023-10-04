71244e44b0cb680033024d1edd297013944f1796
Praise Olayemi <funmilayo1302@gmail.com>
2023-10-04 12:44:23 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * set_bit - returns the value of a bit at a given index
+ * @n: The number
+ * @index: the index
+ * Return: The value of the bit at index INDEX
+ *         Otherwise -1 if an error occured
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned int) * 8))
+		return (-1);
+
+	*n |= (1 << index);
+
+	return (1);
+}
